01a7c9fe73a5b2439805cca3f848a1a50e298ba0
snoupyrelete <drob8896@740s-pl-112.local>
2016-11-14 07:25:40 -0700

    s

--- src/array/view/HipsterPanel.java
+++ src/array/view/HipsterPanel.java
@@ -7,12 +7,10 @@ import java.awt.Color;
 public class HipsterPanel extends JPanel
 {
 	private SpringLayout baseLayout;
-	private JComboBox dropDown;
+	private JComboBox<?> dropDown;
 	private JLabel infoLabel;
 	private JButton myButton;
 	private ArrayController baseController;
-
-	
 	
 	public HipsterPanel(ArrayController baseController)
 	{
@@ -21,7 +19,7 @@ public class HipsterPanel extends JPanel
 		this.baseLayout = new SpringLayout();
 		this.myButton = new JButton("Click me");
 		this.infoLabel = new JLabel("WOW words!");
-		this.dropDown = new JComboBox(baseController.getHipsters());
+		this.dropDown = new JComboBox(baseController.getWords());
 		
 		
 		setupPanel();
